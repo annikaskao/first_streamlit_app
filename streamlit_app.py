70296873574f6af4c07bfbb94cee9befed48b329
annikaskao <91435554+annikaskao@users.noreply.github.com>
2023-09-19 14:15:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import snowflake.connector
+import requests
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -26,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Removing line to only show table
 # streamlit.text(fruityvice_response.json())  # just writes the data to the screen
@@ -36,7 +39,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Outputs a table to the screen
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+# import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -47,3 +53,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you lto add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
