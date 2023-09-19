43db279b51d3de31bc1e5fd493b28ec024c5fd26
annikaskao <91435554+annikaskao@users.noreply.github.com>
2023-09-19 11:25:39 -0400

    Update streamlit_app.py
    trying to find why app did not update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# Removing line so we only show table
+# Removing line to only show table
 # streamlit.text(fruityvice_response.json())  # just writes the data to the screen
 
 # Normalizes the json version of the response
