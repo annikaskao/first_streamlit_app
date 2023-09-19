be1296c31a4c3e6af11b82d62bc20b9ecd524236
annikaskao <91435554+annikaskao@users.noreply.github.com>
2023-09-19 16:09:10 -0400

    Update streamlit_app.py
    errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,6 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + add_my_fruit
 
-streamlit.stop()  
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Get Fruit Load List'):
